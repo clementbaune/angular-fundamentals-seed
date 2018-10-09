39d8741f8b508084b9fae5c78a646883ee893ee8
Clément BAUNE <cbaune@sii.fr>
2018-10-09 14:21:27 +0200

    template ref variables

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from  '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -13,8 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule,
-    FormsModule
+    CommonModule
   ],
   bootstrap: [AppComponent]
 })
